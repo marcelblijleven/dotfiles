5cc53cd9e12538f2722d070f312bc44b79730bf7
Marcel Blijleven <marcelblijleven@gmail.com>
2022-04-25 22:43:28 +0200

    add virtualenv settings

---
+++ python/virtualenv.sh
@@ -0,0 +1,7 @@
+# Virtual env
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON="$(brew --prefix)/bin/python3"
+export VIRTUALENVWRAPPER_VIRTUALENV="$(brew --prefix)/bin/virtualenv"
+export PROJECT_HOME=$HOME/code
+source $(brew --prefix)/bin/virtualenvwrapper.sh
+
