c3fd0e8d464ee681bb447fb414b840450834bb4c
Marcel Blijleven <marcelblijleven@gmail.com>
2022-04-30 00:04:05 +0200

    chore: update incorrect tab

--- notes/aliases.sh
+++ notes/aliases.sh
@@ -1,13 +1,13 @@
 note () {
     local notes_dir="~/notes/docs/"
     mkdir -p ${notes_dir}
-	local filename="$1"
-	local existing_filename=$(cd $notes_dir && fzf -q $filename -1 -0)
-	if [ -z "$existing_filename" ]
-	then
-		local fullpath="${notes_dir}${filename}"
-	else
-		local fullpath="${notes_dir}${existing_filename}"
-	fi
-	vim $fullpath
+    local filename="$1"
+    local existing_filename=$(cd $notes_dir && fzf -q $filename -1 -0)
+    if [ -z "$existing_filename" ]
+    then
+        local fullpath="${notes_dir}${filename}"
+    else
+        local fullpath="${notes_dir}${existing_filename}"
+    fi
+    vim $fullpath
 }
