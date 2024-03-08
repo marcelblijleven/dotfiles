14bde7b335acf2b160910ec6436d345686efd8c1
Marcel Blijleven <marcelblijleven@gmail.com>
2024-03-08 21:56:03 +0100

    feat: add nvim configuration

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add gruvbox theme
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim opts
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
