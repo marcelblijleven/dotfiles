4d4b5ea6e615771693ecea3f288b7d7623cc24a3
Marcel Blijleven <marcelblijleven@gmail.com>
2024-10-01 17:05:03 +0200

    chore(lsp): make ruff use filesystem settings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,28 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      vim.schedule(function()
-        vim.cmd("LspStart")
-      end)
-    end,
+    opts = {
+      servers = {
+        ruff = {
+          init_options = {
+            settings = {
+              -- This will make project based pyproject.toml or ruff.toml
+              -- have priority over global and/or editor settings
+              configurationPreference = "filesystemFirst",
+              logLevel = "info",
+              organizeImports = true,
+            },
+          },
+        },
+        pyright = {
+          settings = {
+            pyright = {
+              -- Only use Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+          },
+        },
+      },
+    },
   },
 }
